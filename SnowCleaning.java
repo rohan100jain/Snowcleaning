2858c46b275da52d7d8f00193866046864e722df
Rohan Jain <rohan100jain@gmail.com>
2013-04-05 11:58:02 -0400

    Checking for fine and salary, also keeping snowState

--- SnowCleaning.java
+++ SnowCleaning.java
@@ -43,25 +43,18 @@ class SnowCleaning {
   SnowCleaning() {
   }
 
-  private boolean isLocationSnowed(int row, int col, int[] snowFalls) {
-    int K = snowFalls.length / 2;
-    for (int i = 0; i < K; ++i) {
-      int row1 = snowFalls[2 * i];
-      int col1 = snowFalls[2 * i + 1];
-      if (row == row1 && col == col1) return true;
-    }
-    return false;
+  private boolean isLocationSnowed(int row, int col) {
+    return (snowState_[row][col] == 1);
   }
 
   private MoveData nearestNotRequiredHelper(int start_row, int start_col,
-      int end_row, int end_col, String dir, ArrayList<Integer> moved_workers,
-      int[] snowFalls) {
+      int end_row, int end_col, String dir, ArrayList<Integer> moved_workers) {
     int worker = workerState_[start_row][start_col];
     if (moved_workers.contains(worker)) {
       return null;
     }
     if (worker != -1) {
-      if (!isLocationSnowed(start_row, start_col, snowFalls)) {
+      if (!isLocationSnowed(start_row, start_col)) {
         return new MoveData(worker, start_row, start_col, end_row, end_col, dir);
       }
     }
@@ -69,31 +62,31 @@ class SnowCleaning {
   }
 
   private MoveData nearestNotRequiredWorker(int row, int col,
-      ArrayList<Integer> moved_workers,int[] snowFalls) {
+      ArrayList<Integer> moved_workers) {
     if (col != 0) {
       MoveData move_data = nearestNotRequiredHelper(row, col - 1, row, col,
-          "R", moved_workers, snowFalls);
+          "R", moved_workers);
       if (move_data != null) {
         return move_data;
       }
     }
     if (col != (boardSize_ - 1)) {
       MoveData move_data = nearestNotRequiredHelper(row, col + 1, row, col,
-          "L", moved_workers, snowFalls);
+          "L", moved_workers);
       if (move_data != null) {
         return move_data;
       }
     }
     if (row != 0) {
       MoveData move_data = nearestNotRequiredHelper(row - 1, col, row, col,
-          "D", moved_workers, snowFalls);
+          "D", moved_workers);
       if (move_data != null) {
         return move_data;
       }
     }
     if (row != (boardSize_ - 1)) {
       MoveData move_data = nearestNotRequiredHelper(row + 1, col, row, col,
-          "U", moved_workers, snowFalls);
+          "U", moved_workers);
       if (move_data != null) {
         return move_data;
       }
@@ -111,6 +104,7 @@ class SnowCleaning {
     for (int i = 0; i < boardSize_; ++i) {
       for (int j = 0; j < boardSize_; ++j) {
         workerState_[i][j] = -1;
+        snowState_[i][j] = 0;
       }
     }
     return 0;
@@ -118,6 +112,9 @@ class SnowCleaning {
 
   public String[] nextDay(int[] snowFalls) {
     ArrayList<String> commands = new ArrayList<String>();
+    if (snowFine_ < salary_) {
+      return commands.toArray(new String[commands.size()]);
+    }
     ArrayList<HireData> hiring_commands = new ArrayList<HireData>();
     ArrayList<MoveData> moving_commands = new ArrayList<MoveData>();
     ArrayList<Integer> moved_workers = new ArrayList<Integer>();
@@ -125,31 +122,43 @@ class SnowCleaning {
     for (int i = 0; i < K; ++i) {
       int row = snowFalls[2 * i];
       int col = snowFalls[2 * i + 1];
-      boolean hasWorker = (workerState_[row][col] != -1);
-      if (!hasWorker) {
-        MoveData move_data = nearestNotRequiredWorker(row, col, moved_workers,
-            snowFalls);
-        if (move_data != null) {
-          commands.add("M " + move_data.id + " " + move_data.dir);
-          moved_workers.add(move_data.id);
-          moving_commands.add(move_data);
-        } else {
-          if (numWorkers_ < 100) {
-            commands.add("H " + row + " " + col);
-            hiring_commands.add(new HireData(row, col, numWorkers_));
-            numWorkers_++;
+      snowState_[row][col] = 1;
+    }
+    for (int row = 0; row < boardSize_; ++row) {
+      for (int col = 0; col < boardSize_; ++col) {
+        if (snowState_[row][col] == 0) continue;
+        boolean hasWorker = (workerState_[row][col] != -1);
+        if (!hasWorker) {
+          MoveData move_data = nearestNotRequiredWorker(row, col,
+              moved_workers);
+          if (move_data != null) {
+            commands.add("M " + move_data.id + " " + move_data.dir);
+            moved_workers.add(move_data.id);
+            moving_commands.add(move_data);
+          } else {
+            if (numWorkers_ < 100) {
+              commands.add("H " + row + " " + col);
+              hiring_commands.add(new HireData(row, col, numWorkers_));
+              numWorkers_++;
+            }
           }
         }
       }
     }
     for (HireData hire_data : hiring_commands) {
       workerState_[hire_data.row][hire_data.col] = hire_data.id;
+      if (snowState_[hire_data.row][hire_data.col] == 1) {
+        snowState_[hire_data.row][hire_data.col] = 0;
+      }
       System.err.println("Added worker " + hire_data.id +
           " to " + hire_data.row + " " + hire_data.col);
     }
     for (MoveData move_data : moving_commands) {
       workerState_[move_data.start_row][move_data.start_col] = -1;
       workerState_[move_data.end_row][move_data.end_col] = move_data.id;
+      if (snowState_[move_data.end_row][move_data.end_col] == 1) {
+        snowState_[move_data.end_row][move_data.end_col] = 0;
+      }
       System.err.println("Moved worker " + move_data.id + " from " +
           move_data.start_row + " " + move_data.start_col + " to " +
           move_data.end_row + " " + move_data.end_col);
